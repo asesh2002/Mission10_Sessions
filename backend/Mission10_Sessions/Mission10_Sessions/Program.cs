60c1f196e0b902c6c75712560f965411ad9434be
asesh2002 <asesh16@byu.edu>
2025-03-14 17:39:54 -0600

    scaffolding commit

--- backend/Mission10_Sessions/Mission10_Sessions/Program.cs
+++ backend/Mission10_Sessions/Mission10_Sessions/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Mission10_Sessions.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<BowlingLeagueContext>(options =>
+{
+    options.UseSqlite(builder.Configuration["ConnectionStrings:BowlerConnection"]);
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
